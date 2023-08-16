364362558e94023e717b389e833e4cdbb7c77ce7
Ishika Tarin Ime <64859699+IshikaIme@users.noreply.github.com>
2023-08-17 02:16:19 +0600

    hii

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import App from './App';
-import { render } from 'react-dom';
-
-render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root"));
