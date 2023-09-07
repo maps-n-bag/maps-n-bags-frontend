c0741bb19ddef8bf78afb8a055e849b6552b0bf9
Ishika Tarin Ime <64859699+IshikaIme@users.noreply.github.com>
2023-09-08 01:47:30 +0600

    navigate with param checked

--- src/Explore/exploreMainPage.js
+++ src/Explore/exploreMainPage.js
@@ -17,112 +17,8 @@ import TimeLine from "../Profile/timeline";
 import cp from "../photos/c.png";
 import pp from "../photos/para.png";
 import cr from "../photos/cr.png";
+import { useParams } from "react-router-dom";
 const baseURL = process.env.REACT_APP_BASE_URL;
-const RowOfBoxes = () => {
-  return (
-    <Box display="flex" justifyContent="center">
-      <Link to={`/`}>
-        <Box
-          width="100%"
-          bgcolor="rgba(245, 230, 83, 0.8)"
-          m={5}
-          p={6}
-       
-        >
-          <div>
-            <img
-              src={cp}
-              style={{
-                fontFamily: "Special Elite",
-                //fontSize: "20px",
-                width: "200px",
-                marginLeft: "2%",
-                marginRight: "2%",
-                //color: "white",
-              }}
-            />
-            <Typography
-              color="black"
-              style={{
-                fontFamily: "Special Elite",
-                fontSize: "150%",
-                textAlign: "center",
-              }}
-            >
-              View Side Tours
-            </Typography>
-          </div>
-        </Box>
-      </Link>
-      <Link to={`/`}>
-        <Box
-          width="100%"
-          bgcolor="rgba(300, 77, 0, 0.8)"
-          m={5}
-          p={6}
-      
-        >
-          <div>
-            <img
-              src={pp}
-              style={{
-                fontFamily: "Special Elite",
-                //fontSize: "20px",
-                width: "130px",
-                marginLeft: "10%",
-                //color: "white",
-              }}
-            />
-            <Typography
-              color="black"
-              style={{
-                fontFamily: "Special Elite",
-                fontSize: "150%",
-                // textAlign: "center",
-              }}
-            >
-              Things to Do Here
-            </Typography>
-          </div>
-        </Box>
-      </Link>
-      <Link to={`/`}>
-        <Box
-          width="100%"
-          bgcolor="rgba(0, 230, 83, 0.8)"
-          m={5}
-          p={6}
-      
-        >
-          <div>
-            <img
-              src={cr}
-              style={{
-                fontFamily: "Special Elite",
-                //fontSize: "20px",
-                width: "175px",
-
-                marginLeft: "20%",
-                //color: "white",
-              }}
-            />
-            <Typography
-              color="black"
-              style={{
-                fontFamily: "Special Elite",
-                fontSize: "150%",
-                textAlign: "center",
-              }}
-            >
-              View Others' Plans
-            </Typography>
-          </div>
-        </Box>
-      </Link>
-    </Box>
-  );
-};
-
 const useStyles = makeStyles((theme) => ({
   bx: {
     alignContent: "center",
@@ -148,17 +44,99 @@ const useStyles = makeStyles((theme) => ({
   },
 
   boxrow: {
-
-     marginBottom: "10%",
+    marginBottom: "10%",
   },
-
-
 }));
-
 export default function ExploreMain() {
   const classes = useStyles();
-  //const {id} =useParams();
+  const { plan_id } = useParams();
   const navigate = useNavigate();
+  const RowOfBoxes = () => {
+    return (  
+      <Box display="flex" justifyContent="center">
+        <Link to={`/`}>
+          <Box width="100%" bgcolor="rgba(245, 230, 83, 0.8)" m={5} p={6}>
+            <div>
+              <img
+                src={cp}
+                style={{
+                  fontFamily: "Special Elite",
+                  //fontSize: "20px",
+                  width: "200px",
+                  marginLeft: "2%",
+                  marginRight: "2%",
+                  //color: "white",
+                }}
+              />
+              <Typography
+                color="black"
+                style={{
+                  fontFamily: "Special Elite",
+                  fontSize: "150%",
+                  textAlign: "center",
+                }}
+              >
+                View Side Tours
+              </Typography>
+            </div>
+          </Box>
+        </Link>
+        <Link to={`/ThingsToDo/${plan_id}`}>
+          <Box width="100%" bgcolor="rgba(300, 77, 0, 0.8)" m={5} p={6}>
+            <div>
+              <img
+                src={pp}
+                style={{
+                  fontFamily: "Special Elite",
+                  //fontSize: "20px",
+                  width: "130px",
+                  marginLeft: "10%",
+                  //color: "white",
+                }}
+              />
+              <Typography
+                color="black"
+                style={{
+                  fontFamily: "Special Elite",
+                  fontSize: "150%",
+                  // textAlign: "center",
+                }}
+              >
+                Things to Do Here
+              </Typography>
+            </div>
+          </Box>
+        </Link>
+        <Link to={`/`}>
+          <Box width="100%" bgcolor="rgba(0, 230, 83, 0.8)" m={5} p={6}>
+            <div>
+              <img
+                src={cr}
+                style={{
+                  fontFamily: "Special Elite",
+                  //fontSize: "20px",
+                  width: "175px",
+
+                  marginLeft: "20%",
+                  //color: "white",
+                }}
+              />
+              <Typography
+                color="black"
+                style={{
+                  fontFamily: "Special Elite",
+                  fontSize: "150%",
+                  textAlign: "center",
+                }}
+              >
+                View Others' Plans
+              </Typography>
+            </div>
+          </Box>
+        </Link>
+      </Box>
+    );
+  };
 
   return (
     <div className={classes.root}>
