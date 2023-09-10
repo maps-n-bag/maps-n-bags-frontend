f8d464bff5a9ca1b46c5ae6a9fcb28c324d5351c
Nazmul Islam Ananto <nazmulislamananto@gmail.com>
2023-09-10 22:42:12 +0600

    others plan design

--- src/OthersPlan/othersPlanCard.js
+++ src/OthersPlan/othersPlanCard.js
@@ -48,7 +48,7 @@ const OthersPLanCard = (props) => {
     };
 
     return (
-        <Grid item xs={12} sm={6} md={4}>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
             <Card sx={{ maxWidth: 345 }}>
                 <CardActionArea >
                     <CardMedia
